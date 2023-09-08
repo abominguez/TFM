1904b5159f798168df923a92ac0f65603609c7df
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-09-08 14:55:33 +0200

    add bar_plot to final consumption prediction

--- Sankey diagram/Sankey diagram/Sankey_functions.py
+++ Sankey diagram/Sankey diagram/Sankey_functions.py
@@ -13,23 +13,31 @@ import streamlit as st
 
 
 #%% Sankey Plot
-def Plot(sheet_name, label, source, target, value):
+def Plot(year,db):
+    label=db[year]["label"]
+    source=db[year]["source"]
+    target=db[year]["target"]
+    value=db[year]["value"]
+    color_nodes=db[year]["color_nodes"]
+    color_links=db[year]["color_links"]
     fig = go.Figure(data=[go.Sankey(
         node = dict(
           pad = 30,
           thickness = 20,
           line = None, # dict(color = "black", width = 0.5),
           label = label,
-          color = None # "blue"
+          color = color_nodes
         ),
         link = dict(
           source = source, # indices correspond to labels, eg A1, A2, A1, B1, ...
           target = target,
-          value = value
+          value = value,
+            color=color_links
       ))])
 
-    fig.update_layout(title_text=sheet_name, font_size=10)
+    fig.update_layout(title_text=year, font_size=10)
     fig.show()
+
 
 #%%
 def Data_Generate(Dict):
@@ -62,9 +70,7 @@ def Data_Generate(Dict):
         #df.fillna(0, inplace=True)
 
         # define the combinations
-        Transformers=['REFINERIES', 'POWER PLANTS', 'SELF-PRODUCERS',
-               'GAS PLANTS', 'CHARCOAL PLANTS', 'COKE PLANTS AND BLAST FURNACES',
-               'DISTILLERIES', 'OTHER CENTERS']
+        Transformers=['REFINERIES', 'POWER PLANTS', 'SELF-PRODUCERS','GAS PLANTS', 'CHARCOAL PLANTS', 'COKE PLANTS AND BLAST FURNACES','DISTILLERIES', 'OTHER CENTERS']
         Primaries=['OIL','NATURAL GAS','COAL','HYDROENERGY','GEOTHERMAL','NUCLEAR','FIREWOOD','SUGARCANE AND PRODUCTS','OTHER PRIMARY']
         Secondaries=['ELECTRICITY','LPG','GASOLINE/ALCOHOL','KEROSENE/JET FUEL','DIESEL OIL','FUEL OIL','COKE','CHARCOAL','GASES','OTHER SECONDARY']
         Consumptions=['TRANSPORT','INDUSTRIAL','RESIDENTIAL','COMMERCIAL, SERVICES, PUBLIC','AGRICULTURE, FISHING AND MINING','CONSTRUCTION AND OTHERS']
@@ -83,17 +89,23 @@ def Data_Generate(Dict):
 
         #get the final consumption column     
 
-        for i in Primaries+Secondaries:
-                unique_combinations.append((i, i+'-FINAL CONSUMPTION',abs(df['FINAL CONSUMPTION'][i])))
+        #for i in Primaries+Secondaries:
+        #        unique_combinations.append((i, i+'-FINAL CONSUMPTION',abs(df['FINAL CONSUMPTION'][i])))
 
         # the final consumption column - Usage column
-        for i in Primaries+Secondaries:
-            for j in Consumptions:
-                unique_combinations.append((i+'-FINAL CONSUMPTION', j,abs(df['FINAL CONSUMPTION'][i])))
-	        
+        for i in Consumptions:
+            for j in Primaries+Secondaries:
+                unique_combinations.append((j, i,abs(df[i][j])))
+                
 
-        label=Transformers+Primaries+Secondaries+Consumptions+[i+'-FINAL CONSUMPTION' for i in Primaries+Secondaries]
+        label=Transformers+Primaries+Secondaries+Consumptions+Primaries+Secondaries
         
+        #colors
+        color_Transformers=['blue','yellow','green','orange','grey','grey','pink','cyan']
+        color_Primaries=['black','orange','darkgray','lightblue','darkred','red','brown','darkgreen','khaki']
+        color_Secondaries=['yellow','lightgreen','plum','plum','plum','grey','grey','grey','grey']
+        color_Consumptions=['darkmagenta' for i in range(9)]
+        color_nodes=color_Transformers+color_Primaries+color_Secondaries+color_Consumptions+color_Primaries+color_Secondaries
         
         # Rename the sheet_name to contain only year. Sample: "1970 - Brazil"--> "1970"
         sheet_name_new = sheet_name.split(' - ')[0]
@@ -103,6 +115,9 @@ def Data_Generate(Dict):
         _dict["target"]=[]
         _dict["value"]=[]
         _dict["label"]=label
+        _dict["color_nodes"]=color_nodes
+        _dict["color_links"]=[]
+        
 
 
         #Dict_out[sheet_name_new] = data  # Store the data in the dictionary
@@ -110,7 +125,7 @@ def Data_Generate(Dict):
             _dict["source"].append(label.index(k[0]))
             _dict["target"].append(label.index(k[1]))
             _dict["value"].append(k[2])
-           
+            _dict["color_links"].append(color_nodes[label.index(k[0])])
     return Dict_out
 
 
@@ -118,36 +133,19 @@ def Data_Generate(Dict):
 
 Dict=pd.read_excel("Brazil_Energy balance matrix.xlsx", sheet_name=None, skiprows=range(4), skipfooter=3)
 
-Dict_out=Data_Generate(Dict)
+db=Data_Generate(Dict)
 
-#sheet_name="2021"
-#label=Dict_out[sheet_name]["label"]
-#source=Dict_out[sheet_name]["source"]
-#target=Dict_out[sheet_name]["target"]
-#value=Dict_out[sheet_name]["value"]
+#Plot(year='1980',db=db)
 
+year= st.number_input("Select year", value=2010, min_value= 1970, max_value=2021, step=1)
+year=str(year)
 
+Plot(year,db)
 
-#Plot(sheet_name=sheet_name, label=label, source=source, target=target, value=value)
-
+st.pyplot(Plot(year,db))
 
 
-# Define a list to store the generated Plotly figures
-all_figures = []
 
-# Get the list of available years from Dict_out keys
-key_options = list(Dict_out.keys())
 
-# Create a multiselect widget to select years
-selected_years = st.multiselect("Select years", options=key_options)
 
-# Generate Plotly figures for selected years and store them in the all_figures list
-for year in selected_years:
-    fig = Plot(sheet_name=year, label=Dict_out[year]["label"], source=Dict_out[year]["source"], 
-               target=Dict_out[year]["target"], value=Dict_out[year]["value"])
-    all_figures.append(fig)
-
-# Display all the stored figures using st.plotly_chart
-for fig in all_figures:
-    st.plotly_chart(fig)
 
